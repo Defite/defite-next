5ae783d5789f59c1df06be25b457c0363506ff14
Nikita Makhov <me@defite.ru>
2025-12-21 02:16:35 +0300

    feat: add xmas tree logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import LogoSvg from '@/icons/logo.svg';
+import LogoNewYearSvg from '@/icons/new-year-logo.svg';
 import Link from 'next/link';
 
 type Props = {
@@ -10,7 +10,7 @@ export const Logo = ({ onClick }: Props) => {
   return (
     <>
       <Link href='/' aria-label='D.' onClick={onClick}>
-        <LogoSvg className='text-neutral-700 dark:text-neutral-300' />
+        <LogoNewYearSvg className='text-neutral-700 dark:text-neutral-300' />
       </Link>
     </>
   );
