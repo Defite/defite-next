fd52f2f7f74e85bb6a394f3e974a083be7c9a276
Nikita Makhov <me@defite.ru>
2025-12-21 21:31:16 +0300

    fix: lazy load for calendar

--- src/components/GithubActivity/GithubActivity.tsx
+++ src/components/GithubActivity/GithubActivity.tsx
@@ -1,19 +1,28 @@
 'use client';
 
 import { useTheme } from 'next-themes';
-import { GitHubCalendar } from 'react-github-calendar';
+import dynamic from 'next/dynamic';
+import { Suspense } from 'react';
+
+// Dynamically import the component with SSR disabled
+const GitHubCalendar = dynamic(
+  () => import('react-github-calendar').then((mod) => mod.GitHubCalendar),
+  {
+    ssr: false,
+  }
+);
 
 export const GithubActivity = () => {
-  const { theme } = useTheme();
+  const { resolvedTheme } = useTheme();
+
+  const colorScheme = resolvedTheme === 'dark' ? 'dark' : 'light';
 
   return (
     <section className='flex flex-col gap-8'>
-    <h2 className='color-primary'>My Github activity (not proud)</h2>
-      <GitHubCalendar
-        colorScheme={theme === 'dark' ? 'dark' : 'light'}
-        username='Defite'
-        loading={typeof window === 'undefined'}
-      />
+      <h2 className='color-primary'>My Github activity</h2>
+      <Suspense fallback={'Loading...'}>
+        <GitHubCalendar colorScheme={colorScheme} username='Defite' />
+      </Suspense>
     </section>
   );
 };
