d30c7399c6e84d537c215f4eae5543105771ee5e
Nikita Makhov <me@defite.ru>
2025-12-21 20:28:39 +0300

    feat: add image zoom

--- src/components/MdxImage/MdxImage.tsx
+++ src/components/MdxImage/MdxImage.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Image, { ImageProps } from 'next/image';
+import Zoom from 'react-medium-image-zoom'
+import 'react-medium-image-zoom/dist/styles.css'
 import style from './style.module.css';
 
 type Props = ImageProps & {
@@ -10,7 +12,9 @@ export function MdxImage(props: Props) {
   const { caption, alt } = props;
   return (
     <figure className={style.figure}>
-      <Image {...props} alt={alt || caption || 'There was an image here'} />
+      <Zoom>
+        <Image {...props} alt={alt || caption || 'There was an image here'} />
+      </Zoom>
       {caption ? (
         <figcaption className={style.figcaption}>{caption}</figcaption>
       ) : null}
