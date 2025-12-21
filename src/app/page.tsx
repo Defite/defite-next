e0671fa5df7b6d5882682755a45981f78205d482
Nikita Makhov <me@defite.ru>
2025-12-21 20:54:07 +0300

    feat: added github calendar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
+import { GithubActivity } from '@/components/GithubActivity/GithubActivity';
 import { Hero } from '@/components/Hero';
 import { PostsList } from '@/components/PostsList';
 import { getBlogPosts } from '@/utils';
 
 export default async function Home() {
   const posts = await getBlogPosts();
-
+  
   return (
     <main className='wrapper mx-auto flex flex-col gap-20 px-3 py-16 lg:px-0'>
       <Hero />
       <PostsList title='Recent posts' posts={posts} />
+      <GithubActivity />
     </main>
   );
 }
---
+++ src/components/GithubActivity/GithubActivity.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { useTheme } from 'next-themes';
+import { GitHubCalendar } from 'react-github-calendar';
+
+export const GithubActivity = () => {
+  const { theme } = useTheme();
+
+  return (
+    <section className='flex flex-col gap-8'>
+    <h2 className='color-primary'>My Github activity (not proud)</h2>
+      <GitHubCalendar
+        colorScheme={theme === 'dark' ? 'dark' : 'light'}
+        username='Defite'
+      />
+    </section>
+  );
+};
