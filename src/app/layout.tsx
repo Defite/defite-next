5657d8d3055c0134bbadbb76df0b38af1122d0c9
Nikita Makhov <defite@gmail.com>
2026-02-12 21:27:42 +0000

    fix: remove SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
           <Header />
           {children}
         </ThemeProvider>
-        <SpeedInsights />
+        {/* <SpeedInsights /> */}
       </body>
     </html>
   );
