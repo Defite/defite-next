3462c7823f6e7ecb9917828a266a6184a7f7fdee
Nikita Makhov <me@defite.ru>
2026-03-08 21:03:38 +0300

    fix: throw out xmas tree, it's March already)

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,4 +1,4 @@
-import LogoNewYearSvg from '@/icons/new-year-logo.svg';
+import SiteLogo from '@/icons/logo.svg';
 import Link from 'next/link';
 
 type Props = {
@@ -9,7 +9,7 @@ export const Logo = ({ onClick }: Props) => {
   return (
     <>
       <Link href='/' aria-label='D.' onClick={onClick}>
-        <LogoNewYearSvg className='text-neutral-700 dark:text-neutral-300' />
+        <SiteLogo className='text-neutral-700 dark:text-neutral-300' />
       </Link>
     </>
   );
